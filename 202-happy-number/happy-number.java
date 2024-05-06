1f8b23487cb2466c3e14a5b9882bea5e830fc771
sarah-haddix <81275285+sarah-haddix@users.noreply.github.com>
2024-05-06 17:40:37 -0400

    Time: 1 ms (64.33%) | Memory: 40.6 MB (46.02%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        while(true) {
+            slow = findSquareNums(slow);
+            fast = findSquareNums(findSquareNums(fast));
+
+            if(fast == slow) { break; }
+        }
+
+        return slow == 1;
+    }
+
+    public int findSquareNums(int n) {
+        int sum = 0;
+
+        while(n > 0) {
+            int place = n %10;
+            sum += (place*place);
+            n = (int)(n / 10);
+        }
+
+        return sum;
+    }
+}
