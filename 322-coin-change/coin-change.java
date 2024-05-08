c6d3d331c903775dcdfe560b636da052501bd8c5
sarah-haddix <81275285+sarah-haddix@users.noreply.github.com>
2024-05-08 09:48:14 -0400

    Time: 22 ms (39.44%) | Memory: 44 MB (85.16%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,24 +1,18 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(amount == 0) {
-            return 0;
-        }
-
-        // arr[i] is min # of coins to get to i amount
+        // 0 ways to get 0 coins, no ways to get i coins yet
         int[] memo = new int[amount+1];
-        for(int i = 0; i < memo.length; i++) {
-            memo[i] = -1; //-1 means not feasible
+        for(int i = 1; i < memo.length; i++) {
+            memo[i] = -1;
         }
 
-        memo[0] = 0; // memo: 0, -1, 1, -1
-        for(int i = 1; i < amount+1; i++) { //i=3
-            for(int coin = 0; coin < coins.length; coin++) {
-                int subproblem = i - coins[coin]; //subproblem = 1
-                if(subproblem >= 0) {
-                    if(!(memo[i] == -1 && memo[subproblem] == -1)) {
-                    if(memo[i] == -1) { memo[i] = memo[subproblem] + 1; }
-                    else if(memo[subproblem] == -1) {}
-                    else {memo[i] = Math.min(memo[i], memo[subproblem] + 1); }
+        for(int i = 1; i <= amount; i++) { //ways to get each amount of coins, counting up from 1
+            for(int j = 0; j < coins.length; j++) { //iterate through coins
+                if(i - coins[j] >= 0) { //feasible
+                    if(memo[i] != -1 || memo[i-coins[j]] != -1) {
+                    if(memo[i] != -1 && memo[i-coins[j]] != -1) {memo[i] = Math.min(memo[i], memo[i-coins[j]] + 1);}
+                    if(memo[i] == -1) { memo[i] = memo[i-coins[j]] + 1; }
+                    if(memo[i-coins[j]] == -1) { memo[i] = memo[i]; }
                     }
                 }
             }
@@ -27,3 +21,6 @@ class Solution {
         return memo[amount];
     }
 }
+
+// using dp approach with memoization
+// keep an array of ways to get to i amount of money
