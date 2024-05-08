bd35fd2fae778133131a19075723ea3194617567
sarah-haddix <81275285+sarah-haddix@users.noreply.github.com>
2024-05-07 21:50:19 -0400

    Time: 0 ms (100.00%) | Memory: 40.2 MB (46.49%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,24 +1,21 @@
 class Solution {
     public int climbStairs(int n) {
-        // dp problem. could do it with recursion or tabulation, but since we only
-        // need to remember the previous 2 steps, we can just use two variables
+        if(n == 1) { return 1; } //take care of stair 1
 
-        if(n == 1) { return 1; }
+        int prevStep = 1; 
+        int currStep = 1; 
 
-        int prev = 1;
-        int curr = 1;
-
-        //1 taken care of, start at 2 and add until you get to n
-        for(int i = 2; i <= n; i++) {
-            // new = new-1 + new-2
-            int temp = curr;
-            curr = prev + curr;
-            prev = temp;
+        for(int i = 2; i <= n; i++) { // i = 2 refers to stair 2
+            int temp = currStep;
+            currStep = currStep + prevStep;
+            prevStep = temp;
         }
 
-        return curr;
+        return currStep;
     }
 }
 
-// 2 ways to get to stair n - taking 1 step from stair n-1 or 2 steps from n-2
-// therefore, 
+// similar to fs
+// 2 ways to get to where you are, from n-1 or n-2
+// bottom up dp approach
+// find ways to get to 
