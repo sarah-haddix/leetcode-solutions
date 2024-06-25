f168102d1c36772ab97dec0e431d5695f3d5e28b
sarah-haddix <81275285+sarah-haddix@users.noreply.github.com>
2024-06-25 01:48:20 -0600

    Time: 0 ms (100.00%) | Memory: 42.1 MB (30.42%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,26 +1,28 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // start - put everything into nums1
-        // nums1 - m, nums2 - n
-        
-        m--;
-        int i = m+n;
-        n--;
-
-        while(n >= 0 && i >= 0) {
-            if(m >= 0) {
-            if(nums1[m] > nums2[n]) {
-                nums1[i] = nums1[m];
-                m--;
-            } else {
-                nums1[i] = nums2[n];
-                n--;
+        int j = n-1;
+        int i = m-1;
+        int f = m+n-1;
+        while(j >= 0 && i >= 0) {
+            if(nums1[i] > nums2[j]) {
+                nums1[f] = nums1[i];
+                f--;
+                i--;
             }
-        } else {
-            nums1[i] = nums2[n];
-            n--;
+            else {
+                nums1[f] = nums2[j];
+                f--;
+                j--;
+            }
         }
-            i--;
+
+        while(j >= 0) {
+            nums1[f] = nums2[j];
+            f--;
+            j--; 
         }
     }
 }
+// increasing
+// nums1 - 1 2 5 6 _ _ _, m=4, i
+// nums2 - 3 4 8, n=3, j
