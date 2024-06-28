d17b55151376003507cd3ad8b8f9e36135d8d787
sarah-haddix <81275285+sarah-haddix@users.noreply.github.com>
2024-06-28 05:22:44 -0400

    Time: 0 ms (100.00%) | Memory: 41.8 MB (34.54%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,25 +1,12 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int i = 0;
-        int j = nums.length-1;
-
-        while(i <= j) {
-            // j points to the next available spot
-            while(i <= j && nums[j] == val) {
-                j--;
+        int j = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != val) {
+                nums[j] = nums[i];
+                j++;
             }
-
-            if(i >= j) {
-                return j+1;
-            }
-
-            if(nums[i] == val) {
-                nums[i] = nums[j];
-                j--;
-            }
-            i++;
         }
-
-        return j+1;
+        return j;
     }
 }
