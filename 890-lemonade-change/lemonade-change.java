ee1b5dceaa4ecc57eba403a94e55f8d6baf2d348
sarah-haddix <81275285+sarah-haddix@users.noreply.github.com>
2024-08-15 09:41:01 -0400

    Time: 2 ms (95.27%) | Memory: 55.9 MB (36.83%) - LeetSync

--- 890-lemonade-change/lemonade-change.java
+++ 890-lemonade-change/lemonade-change.java
@@ -1,32 +1,29 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        int[] change = new int[2]; // 0: $5, 1: $10
-        for(int i = 0; i < bills.length; i++) {
-            System.out.println("Bills[i]: " + bills[i]);
-            System.out.println("5s: " + change[0]);
-            System.out.println("10s: " + change[1]);
-            if(bills[i] == 5) { change[0]++; }
-            // bills[i] == 10
-            else if(bills[i] == 10){
-                // need a 5 in change
-                change[0]--;
-                if(change[0] < 0) { return false; }
-                change[1]++;
-            }
-            // bills[i] == 20
-            else {
-                // need a 10 and a 5 or 3 5s
-                // since 10s can't be used for anything else, use these first
-                if(change[1] > 0 && change[0] > 0) {
-                    change[1]--;
-                    change[0]--;
+        int five_dollars = 0, ten_dollars = 0;
+
+        for (int x : bills) {
+            if (x == 5) {
+                five_dollars++;
+            } else if (x == 10) {
+                if (five_dollars > 0) {
+                    five_dollars--;
+                    ten_dollars++;
+                } else {
+                    return false;
                 }
-                else if(change[0] >= 3) {
-                    change[0] -= 3;
+            } else {
+                if (five_dollars > 0 && ten_dollars > 0) {
+                    five_dollars--;
+                    ten_dollars--;
+                } else if (five_dollars > 2) {
+                    five_dollars -= 3;
+                } else {
+                    return false;
                 }
-                else { return false; }
             }
         }
+
         return true;
     }
 }
